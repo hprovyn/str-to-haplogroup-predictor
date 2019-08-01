bb5ca396f877654e31cad0a882ffabaab997db0f
root <root@s192-169-140-215.secureserver.net>
2019-08-01 09:26:22 +0000

    made file separator a config var

--- runPredict.sh
+++ runPredict.sh
@@ -1,15 +1,16 @@
 #SET PARAMS FROM CONFIG
-CFG_FILE=config.txt
+#THIS MUST BE ABSOLUTE PATH TO CONFIG FILE
+CFG_FILE=/var/lib/str-to-haplogroup-predictor/config.txt
 CFG_CONTENT=$(cat $CFG_FILE | sed -r '/[^=]+=[^=]+/!d' | sed -r 's/\s+=\s/=/g')
 eval "$CFG_CONTENT"
 
 PATH=$PATH:$pythonPath
 
-rfPredict_py="$pythonScriptsDir\RF_predict.py"
+rfPredict_py="$pythonScriptsDir${pathSeparator}RF_predict.py"
 
-dataDir="$experimentDir\data"
-panelHierarchyFile="$experimentDir\panelHierarchy.csv"
-predictionPolicyFileStem="$experimentDir\predictionPolicy"
-modelPickleFileStem="$experimentDir\model"
+dataDir="$experimentDir${pathSeparator}data"
+panelHierarchyFile="$experimentDir${pathSeparator}panelHierarchy.csv"
+predictionPolicyFileStem="$experimentDir${pathSeparator}predictionPolicy"
+modelPickleFileStem="$experimentDir${pathSeparator}model"
 
 python "$rfPredict_py" "$dataDir" "$panelHierarchyFile" "$predictionPolicyFileStem" "$modelPickleFileStem" "$percentMissingSTRThreshold" $1
