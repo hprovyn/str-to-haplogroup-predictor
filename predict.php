b1aaa7c480042588ae6419f9330915a79b80012f
Hunter <hunterprovyn@gmail.com>
2019-08-01 13:21:00 +0000

    table view of STR input

--- predict.php
+++ predict.php
@@ -5,6 +5,7 @@ STRs input: <?php echo $_POST["input"]; ?>
 <br><br>
 <?php $input = trim($_POST["input"]);
 $parsed=$input;
+$alleles = explode(",", $input);
 if (strpos($input, '=') === false) {
     
 $pieces = explode(" ", $input);
@@ -14,9 +15,19 @@ array_push($alleles, $pieces[$x * 2] . "=" . $pieces[$x * 2 + 1]);
 }
 $parsed = join(",", $alleles);
 }
-echo "STRs parsed: " . $parsed . "<br><br>";
+$parsedTable = "<table border=\"1\">";
+$strRow = "<tr><td>STR</td>";
+$alleleRow = "<tr><td>Allele</td>";
+for ($x = 0; $x < count($alleles); $x++) {
+$alleleExploded = explode("=", $alleles[$x]);
+$strRow = $strRow . "<td>" . $alleleExploded[0] . "</td>";
+$alleleRow = $alleleRow . "<td>" . $alleleExploded[1] . "</td>";
+}
+$parsedTable = $parsedTable . $strRow . "</tr>" . $alleleRow . "</tr></table>";
+echo $parsedTable . "<br><br>";
+
 $message = exec('/var/lib/str-to-haplogroup-predictor/runPredict.sh ' . $parsed);
-echo "Your predicted haplogroup is " . $message; ?>
+echo $message; ?>
 
 </body>
 </html>
