025f466876067bbb6494408ef8b285b72bae1484
Hunter Provyn <hunterprovyn@gmail.com>
2019-10-24 14:11:53 +0400

    removed troubleshooting echo statements

--- index.php
+++ index.php
@@ -85,7 +85,6 @@ outlier cases will be covered and considered for the prediction. We hope that th
             $ftdnaHeaderOrder = array("DYS393","DYS390","DYS19","DYS391","DYS385","DYS426","DYS388","DYS439","DYS389I","DYS392","DYS389II","DYS458","DYS459","DYS455","DYS454","DYS447","DYS437","DYS448","DYS449","DYS464","DYS460","Y-GATA-H4","YCAII","DYS456","DYS607","DYS576","DYS570","CDY","DYS442","DYS438","DYS531","DYS578","DYF395","DYS590","DYS537","DYS641","DYS472","DYF406S1","DYS511","DYS425","DYS413","DYS557","DYS594","DYS436","DYS490","DYS534","DYS450","DYS444","DYS481","DYS520","DYS446","DYS617","DYS568","DYS487","DYS572","DYS640","DYS492","DYS565","DYS710","DYS485","DYS632","DYS495","DYS540","DYS714","DYS716","DYS717","DYS505","DYS556","DYS549","DYS589","DYS522","DYS494","DYS533","DYS636","DYS575","DYS638","DYS462","DYS452","DYS445","Y-GATA-A10","DYS463","DYS441","Y-GGAAT-1B07","DYS525","DYS712","DYS593","DYS650","DYS532","DYS715","DYS504","DYS513","DYS561","DYS552","DYS726","DYS635","DYS587","DYS643","DYS497","DYS510","DYS434","DYS461","DYS435");
         
             $strs = preg_split("/[\t]/", $input);
-            echo $input . "<br>" . $strs . "<br>" . count($strs) . "<br>";          
             $alleles = array();
             for ($x = 0; $x < count($strs); $x++) {
                 if ($ftdnaHeaderOrder[$x] == "Y-GATA-H4") {
