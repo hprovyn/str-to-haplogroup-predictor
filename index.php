e52fbfbd825dc80d09fa548fd3120906ede2d3ab
Hunter Provyn <hunterprovyn@gmail.com>
2019-12-18 16:30:59 +0400

    show warning for unrecognized STRs input into form

--- index.php
+++ index.php
@@ -74,6 +74,7 @@ outlier cases will be covered and considered for the prediction. We hope that th
 
 <?php if(isset($_POST['input'])) { 
         $corrected = "";
+        $unrecognized = array();
         ?>
         <?php $input = trim($_POST["input"]);
         $parsed=$input;
@@ -84,9 +85,20 @@ outlier cases will be covered and considered for the prediction. We hope that th
                 $pieces = explode(" ", $input);
                 $alleles = array();
                 for ($x = 0; $x < count($pieces) / 2; $x++) {
-                array_push($alleles, $pieces[$x * 2] . "=" . $pieces[$x * 2 + 1]);
+                    array_push($alleles, $pieces[$x * 2] . "=" . $pieces[$x * 2 + 1]);
+                }
+                $parsed = join(",", $alleles);
             }
-            $parsed = join(",", $alleles);
+            $recognized = array("DYS391","DYS389I","DYS437","DYS439","DYS389II","DYS438","DYS426","DYS393","YCAII","DYS390","DYS385","Y-GATA-H4","DYS388","DYS447","DYS19","DYS392","DYS458","DYS455","DYS454","DYS464","DYS448","DYS449","DYS456","DYS576","CDY","DYS460","DYS459","DYS570","DYS607","DYS442","DYS728","DYS723","DYS711","DYR76","DYR33","DYS727","DYR157","DYS713","DYS531","DYS578","DYF395","DYS590","DYS537","DYS641","DYS472","DYF406S1","DYS511","DYS557","DYS490","DYS446","DYS481","DYS413","DYS534","DYS450","DYS425","DYS594","DYS444","DYS520","DYS436","DYS565","DYS572","DYS617","DYS568","DYS487","DYS640","DYS492","DYR112","DYS518","DYS614","DYS626","DYS644","DYS684","DYS710","DYS485","DYS632","DYS495","DYS540","DYS714","DYS716","DYS717","DYS505","DYS556","DYS549","DYS589","DYS522","DYS494","DYS533","DYS636","DYS575","DYS638","DYS462","DYS452","DYS445","Y-GATA-A10","DYS463","DYS441","Y-GGAAT-1B07","DYS525","DYS712","DYS593","DYS650","DYS532","DYS715","DYS504","DYS513","DYS561","DYS552","DYS726","DYS635","DYS587","DYS643","DYS497","DYS510","DYS434","DYS461","DYS435");
+            $parsedExploded = explode(",", $parsed);
+            for ($x = 0; $x < count($parsedExploded); $x++) {
+                $thisExpl = explode("=", $parsedExploded[$x]);
+                if (in_array($thisExpl[0], $recognized) == FALSE) {
+                    array_push($unrecognized, $thisExpl[0]);
+                }
+            }
+            if (count($unrecognized) > 0) {
+                $corrected = "<br>Unrecognized STR names: " . join(", ", $unrecognized) . "<br>";
             }
         } else {
             $ftdnaHeaderOrder = array("DYS393","DYS390","DYS19","DYS391","DYS385","DYS426","DYS388","DYS439","DYS389I","DYS392","DYS389II","DYS458","DYS459","DYS455","DYS454","DYS447","DYS437","DYS448","DYS449","DYS464","DYS460","Y-GATA-H4","YCAII","DYS456","DYS607","DYS576","DYS570","CDY","DYS442","DYS438","DYS531","DYS578","DYF395","DYS590","DYS537","DYS641","DYS472","DYF406S1","DYS511","DYS425","DYS413","DYS557","DYS594","DYS436","DYS490","DYS534","DYS450","DYS444","DYS481","DYS520","DYS446","DYS617","DYS568","DYS487","DYS572","DYS640","DYS492","DYS565","DYS710","DYS485","DYS632","DYS495","DYS540","DYS714","DYS716","DYS717","DYS505","DYS556","DYS549","DYS589","DYS522","DYS494","DYS533","DYS636","DYS575","DYS638","DYS462","DYS452","DYS445","Y-GATA-A10","DYS463","DYS441","Y-GGAAT-1B07","DYS525","DYS712","DYS593","DYS650","DYS532","DYS715","DYS504","DYS513","DYS561","DYS552","DYS726","DYS635","DYS587","DYS643","DYS497","DYS510","DYS434","DYS461","DYS435");
